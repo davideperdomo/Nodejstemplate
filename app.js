f0dacd675fd7b127fbeef1c463c59b72b7b732ff
David Perdomo <daeperdomocr@unal.edu.co>
2018-10-31 03:36:09 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose');
+
+// Connection to DB
+mongoose.connect('mongodb://daeperdomocr-olympus-6506740/tvshows', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
+});
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// Import Models and controllers
+var models     = require('./models/tvshow')(app, mongoose);
+var TVShowCtrl = require('./controllers/tvshows');
+
+// Example Route
+var router = express.Router();
+router.get('/', function(req, res) {
+  res.send("Hello world!");
+});
+app.use(router);
+
+// API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+// Start server
+app.listen(process.env.PORT);
+console.log('Server running at ' + process.env.IP + ':' + process.env.PORT);
+module.exports = app;
